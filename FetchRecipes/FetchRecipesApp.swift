8963f9a5217382882418a8253b9ea11b8e0e9c6b
Gina Mullins <LittlePeculiar13@gmail.com>
2024-08-17 11:42:55 -0700

    Initial Commit

---
+++ FetchRecipes/FetchRecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchRecipesApp.swift
+//  FetchRecipes
+//
+//  Created by Gina Mullins on 8/17/24.
+//
+
+import SwiftUI
+
+@main
+struct FetchRecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
